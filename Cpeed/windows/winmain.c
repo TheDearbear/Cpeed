02a23eb9c967f06a4a845218f2dc8d183a460ab7
Oleg <37769766+TheDearbear@users.noreply.github.com>
2025-01-25 19:57:27 +0300

    Enable surface extensions for Windows

--- Cpeed/windows/winmain.c
+++ Cpeed/windows/winmain.c
@@ -36,7 +36,19 @@ const CpdPlatformExtensions* PLATFORM_alloc_vulkan_instance_extensions() {
         return 0;
     }
 
-    extensions->count = 0;
+    const int extension_count = 2;
+
+    char** extensionNames = (char**)malloc(extension_count * sizeof(char*));
+    if (extensionNames == 0) {
+        free(extensions);
+        return 0;
+    }
+
+    extensions->count = extension_count;
+    extensions->extensions = extensionNames;
+
+    extensionNames[0] = VK_KHR_SURFACE_EXTENSION_NAME;
+    extensionNames[1] = VK_KHR_WIN32_SURFACE_EXTENSION_NAME;
 
     return extensions;
 }
@@ -65,9 +77,6 @@ const CpdPlatformExtensions* PLATFORM_alloc_vulkan_ui_device_extensions() {
 
 void PLATFORM_free_vulkan_extensions(CpdPlatformExtensions* extensions) {
     if (extensions->count > 0) {
-        for (unsigned int i = 0; i < extensions->count; i++) {
-            free(extensions->extensions[i]);
-        }
         free(extensions->extensions);
     }
 
--- Cpeed/windows/winmain.h
+++ Cpeed/windows/winmain.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
+#define VK_USE_PLATFORM_WIN32_KHR
 
 #include <windows.h>
 #include <vulkan/vulkan.h>
